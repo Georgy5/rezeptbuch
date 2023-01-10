59c864e8b5310ffe6a042a26aba932d282c8a6d4
Karl Keller <kgk86@hotmail.com>
2023-01-10 14:24:52 +0100

    Create seeds file
    ...using Faker to generate fake data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Use Faker to generate fake data
+require "faker"
+
+# Destroy exisitng Recipes
+puts "Cleaning up database..."
+Recipe.destroy_all
+puts "Database cleaned"
+
+# Create around 5 or more recipes
+puts "Creating 8 fake recipes..."
+8.times do
+  Recipe.create!(
+    title:        Faker::Food.dish,
+    author:       Faker::Name.name,
+    description:  Faker::Food.description
+  )
+end
+puts "Finished!"
