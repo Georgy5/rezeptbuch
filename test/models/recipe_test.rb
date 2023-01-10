198c4734ac309c8278bf9a0872bbc6f0543e2310
Karl Keller <kgk86@hotmail.com>
2023-01-10 14:03:19 +0100

    Setup recipe model validity test

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class RecipeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @recipe  = Recipe.new(title: "Sadza", author: "Karl",
+                          description: "Vermische das Maismehl mit Wasser.")
+  end
+
+  test "should be valid" do
+    assert @recipe.valid?
+  end
+
+  test "title should be present" do
+    @recipe.title = " "
+    assert_not @recipe.valid?
+  end
+
+  test "author should be present" do
+    @recipe.author = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
 end
