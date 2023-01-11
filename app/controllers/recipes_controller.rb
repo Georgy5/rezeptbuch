f0293919b730b39fdcf4edd6f8a9705b3e4469c0
Karl Keller <kgk86@hotmail.com>
2023-01-11 11:52:21 +0100

    Add Show recipes feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,16 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order("created_at DESC")
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.requre(:recipe).permit(:title, :author,
+      :description, :photo)
   end
 end
