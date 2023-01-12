e69d7477f849742e0c3eed150a98c72f99520514
Karl Keller <kgk86@hotmail.com>
2023-01-12 01:29:31 +0100

    Adjust routes in test

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get recipes_index_url
+    get recipes_path
     assert_response :success
   end
 
@@ -10,7 +10,6 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_response :success
     assert_select "title", "Rezeptbuch"
-    assert_select "h1", "Rezeptbuch"
-    assert_select "body", /^Willkommen/
+    assert_select "h1", /^Willkommen/
   end
 end
