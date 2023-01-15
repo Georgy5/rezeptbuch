26c26d4600872b6b4a2a5e177fb06ebdb5c596b2
Karl Keller <kgk86@hotmail.com>
2023-01-15 11:11:43 +0100

    Update route URL paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get "sessions/new"
   root "recipes#index"
   resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :users
   get   "/signup", to: "users#new"
   get   "/login",   to: "sessions#new"
   post  "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
 end
