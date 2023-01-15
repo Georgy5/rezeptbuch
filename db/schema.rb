05ad050f93ada5f96b39ca1a0dbd557dbd7f80a9
Karl Keller <kgk86@hotmail.com>
2023-01-15 06:04:41 +0100

    Update Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_042459) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_050048) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,7 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_042459) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
