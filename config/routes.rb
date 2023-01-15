adf0418a54fdf70f38149f3455853c90d50170ee
Karl Keller <kgk86@hotmail.com>
2023-01-15 08:32:26 +0100

    Add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "recipes#index"
   resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :users
-  get "/signup", to: "users#new"
+  get   "/signup", to: "users#new"
+  get   "/login",   to: "sessions#new"
+  post  "/login",   to: "sessions#create"
 end
