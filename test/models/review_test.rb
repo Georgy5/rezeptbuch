53038dccaad4e560684c075f89b8a69084745780
Karl Keller <kgk86@hotmail.com>
2023-01-24 17:02:41 +0100

    Update review model test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(first_name: 'Example', last_name: 'User', 
+                      email: 'user@example.com', password: "foobar", 
+                      password_confirmation: "foobar")
+    @recipe  = Recipe.new(title: "Sadza", author: @user.name,
+                          description: "Vermische das Maismehl mit Wasser.")
+    @review = Review.new(rating: 3)
+    @review.recipe = @recipe
+    @review.user = @user
+  end
+
+  test "should be valid" do
+    assert @review.valid?
+  end
 end
