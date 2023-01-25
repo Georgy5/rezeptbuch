7af0bb52a9e41381d27e02c42ce61588867bb201
Karl Keller <kgk86@hotmail.com>
2023-01-25 17:23:03 +0100

    Add review controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,24 @@
 class ReviewsController < ApplicationController
+  before_action :logged_in_user, only: [:new]
+  
   def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if @review.save!
+      redirect_to review_path(@review)
+    else
+      flash[:alert] = "Bewertung konnte nicht abgegeben werden."
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :user_id, :recipe_id)
   end
 end
