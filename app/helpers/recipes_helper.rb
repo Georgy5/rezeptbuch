2645db188ddcc727bd1e36d325969fd681e5321c
Karl Keller <kgk86@hotmail.com>
2023-02-07 21:28:10 +0100

    Add helper method to use in the view

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -3,4 +3,12 @@ module RecipesHelper
     reviews = recipe.reviews
     rating = reviews.average(:rating).to_i()
   end
+
+  def pluralize_reviews_de(rating)
+    if rating == 1
+      "#{rating}e Bewertung"
+    else
+      "#{rating} Bewertungen"
+    end
+  end
 end
