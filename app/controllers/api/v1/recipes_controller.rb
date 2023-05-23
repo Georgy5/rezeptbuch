b94b8d0a503389c0783f8147a5749b3f5d04d1ad
Karl Keller <kgk86@hotmail.com>
2023-05-24 00:45:36 +0200

    Add API Show endpoint

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::RecipesController < ActionController::API
     @recipes = Recipe.all.order("created_at DESC")
     render json: @recipes
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render json: @recipe.to_json(
+      :include => {
+        :reviews => {only: [:rating, :user_id]}
+      }
+    )
+  end
 end
