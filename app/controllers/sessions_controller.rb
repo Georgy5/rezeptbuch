5b41acfb2bf3ac39ecd0ad724dcd35a65c9dc63b
Karl Keller <kgk86@hotmail.com>
2023-08-10 08:58:54 +0200

    Update default redirect path on signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       reset_session
       log_in user
-      redirect_back_or user
+      redirect_back_or root_url
     else
       flash.now[:danger] = "Invalid email/password combination."
       render "new", status: :unprocessable_entity
