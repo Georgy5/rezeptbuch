69272e140599de5804d4f4b60d8873235db54a9d
Karl Keller <kgk86@hotmail.com>
2023-08-17 23:20:03 +0200

    Configure I18n locales

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module Rezeptbuch
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = %i[en de]
+    config.i18n.default_locale = :de
   end
 end
