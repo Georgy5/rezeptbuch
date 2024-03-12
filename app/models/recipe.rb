a112136fa91bdd917a03e97dbb906a4c4772101a
Karl Keller <kgk86@hotmail.com>
2024-03-12 17:01:30 +0100

    Show frienldy URLs in latin characters
    Add latinize() method to the Recipe model and
    update to_param method for friendly URLs

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user, optional: true
   has_one_attached :photo
   has_many :reviews, dependent: :destroy
-  
+
   validates :title, presence: true
   validates :author, presence: true
   validates :description, presence: true
@@ -10,6 +10,12 @@ class Recipe < ApplicationRecord
   broadcasts_to -> (recipe) { "recipes" }, inserts_by: :prepend
 
   def to_param
-    "#{id}-#{title.parameterize}"
+    "#{id}-#{latinize(title).parameterize}"
+  end
+
+  def latinize(string)
+    string.unicode_normalize(:nfd).chars.select do |char|
+      char.match(/\p{Latin}|\s/)
+    end.join
   end
 end
